8aeffbfbcc6c0bab685d840a1c742d08d7f5da06
Markus Faerevaag <m@frv.ag>
2015-05-08 16:34:37 +0200

    comment dev code

--- server/init.ts
+++ server/init.ts
@@ -11,16 +11,16 @@ Meteor.startup(() => {
     // Shops.remove({}); // for dev
 
     // parse hardcoded
-    var shops = JSON.parse(Assets.getText('data.json'));
+    // var shops = JSON.parse(Assets.getText('data.json'));
 
-    _.each(shops, (shop) => {
-        Shops.insert({
-            name: shop['name'],
-            lat: parseFloat(shop['lat']),
-            lng: parseFloat(shop['lng']),
-            price: parseInt(shop['price']),
-            selection: shop['selection'],
-            verified: true
-        });
-    });
+    // _.each(shops, (shop) => {
+    //     Shops.insert({
+    //         name: shop['name'],
+    //         lat: parseFloat(shop['lat']),
+    //         lng: parseFloat(shop['lng']),
+    //         price: parseInt(shop['price']),
+    //         selection: shop['selection'],
+    //         verified: true
+    //     });
+    // });
 });
